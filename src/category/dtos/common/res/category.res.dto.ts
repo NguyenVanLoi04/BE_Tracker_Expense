021bd8b477b5beadfa5ab607765c09b1ac5bfe92
NguyenVanLoi2004 <loishy2004@gmail.com>
2026-04-13 14:46:27 +0700

    Map more data category at API default for FE

--- src/category/dtos/common/res/category.res.dto.ts
+++ src/category/dtos/common/res/category.res.dto.ts
@@ -12,12 +12,20 @@ export class CategoryResDto {
   @Expose() name: string;
   @Expose() priority: number;
   @Expose() status: CategoryStatus;
+  @Expose() color: string;
+  @Expose() icon: string;
+  @Expose() isDefault: boolean;
+  @Expose() type: string;
 
   static mapProperty(dto: CategoryResDto, data: Category) {
     dto.id = data.id;
     dto.name = data.name;
     dto.priority = data.priority;
     dto.status = data.status;
+    dto.color = data.color;
+    dto.icon = data.icon;
+    dto.isDefault = data.isDefault;
+    dto.type = data.type;
   }
 
   static forCustomer(params: CategoryResDtoParams) {
